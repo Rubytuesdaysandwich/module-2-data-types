7e4adf3a85d8420fac0378c101d1c078e9ae1bb8
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-05-11 12:48:03 -0600

    integers basic math first commit

---
+++ code-along-assignment/integers.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>integers</title>
+  </head>
+  <body>
+<?php
+$var1 =3;
+$var2 =4;
+?>
+Basic Math: <?php echo((1 + 2 + $var1) * $var2) / 2 - 5;?>
+
+
+
+  </body>
+</html>
