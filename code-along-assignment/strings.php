4bed6e33f7f3ee60ff3c63b169360bd939624d2d
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-05-11 11:35:10 -0600

    strings update

--- code-along-assignment/strings.php
+++ code-along-assignment/strings.php
@@ -12,8 +12,16 @@
     $target = "World";
     $phrase = $greeting . " " . $target;
     echo $phrase;
-
     ?>
+    <br/>
+    <?php
+    
+    echo "$phrase Again<br/>";
+    echo '$phrase Again<br/>';
+    echo "{$phrase}Again<br/>";
+    
+    ?>
+
 
 
 
