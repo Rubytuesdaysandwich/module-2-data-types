cdf6f8583531d7198515c4fd6f7c8ca873e04aee
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-05-11 12:26:59 -0600

    length,trim,find,replace by string

--- code-along-assignment/string_functions.php
+++ code-along-assignment/string_functions.php
@@ -7,7 +7,7 @@
       <?php
       
       $first = "The quick brown fox";
-      $second = " jumped ove the lazy dog.";
+      $second = " jumped over the lazy dog.";
 
       $third = $first;
       $third .=$second;
@@ -20,6 +20,10 @@
       uppercase: <?php echo strtoupper($third);?><br/>
       uppercase first: <?php echo ucfirst($third);?><br/>
       uppercase words: <?php echo ucwords($third);?><br/>
-      
+      <br/>
+      Length: <?php echo strlen($third);?><br/>
+      Trim:<?php echo "A" . trim("B C D") . "E";?><br/>
+      Find:<?php echo strstr($third, "brown");?><br/>
+      Replace by string:<?php echo str_replace("quick","super-fast",$third);?><br/>
   </body>
 </html>
