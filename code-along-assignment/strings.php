82624ba1810c48ffb071e8841a8a3af038742575
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-05-11 11:28:13 -0600

    strings

---
+++ code-along-assignment/strings.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>strings</title>
+  </head>
+  <body>
+    <?php
+    echo"Hello world<br/>";
+    echo'Hello world<br/>';
+
+    $greeting ="Hello";
+    $target = "World";
+    $phrase = $greeting . " " . $target;
+    echo $phrase;
+
+    ?>
+
+
+
+  </body>
+</html>
