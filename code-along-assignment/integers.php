397b20ea2d4b1045851f84bb980ec5848b2077e9
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-05-11 12:55:50 -0600

    math functions

--- code-along-assignment/integers.php
+++ code-along-assignment/integers.php
@@ -9,8 +9,14 @@ $var1 =3;
 $var2 =4;
 ?>
 Basic Math: <?php echo((1 + 2 + $var1) * $var2) / 2 - 5;?>
-
-
+<br/>
+Absolute value:<?php echo abs(0 - 300);?><br/>
+Exponential:<?php echo pow(2,8);?><br/>
+Square Root:<?php echo sqrt(100);?><br/>
+Modulo:<?php echo fmod(20,7);?><br/>
+Random:<?php echo rand();?><br/>
+Random(min,max):<?php echo rand(1,10);?><br/>
+<br/>
 
   </body>
 </html>
