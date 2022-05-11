c71a410def4cf2455e45ea765c97b64cbe6c1488
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-05-11 11:21:20 -0600

    variables

---
+++ code-along-assignment/variables.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>variables</title>
+  </head>
+  <body>
+      <?php
+      
+      $var1 = 10;//assigning var1 to 10
+      echo $var1; //calling var1 with echo to browser displays 10
+   
+      echo"<br/>";//line break
+
+       $var1 = 100;//set var1 to 100
+       echo $var1;//calling var1 to browser with echo displays 100
+      
+       echo "<br/>";//line break    
+
+       $var2 = "Hello world";//assigning var2 Hello world 
+       echo $var2;//calling var2 to browser displays Hello world
+      
+      ?>
+  </body>
+</html>
