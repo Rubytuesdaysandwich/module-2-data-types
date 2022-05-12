0b9fb1f7bfdb9cd765ca9a782ef0873e8a887c85
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-05-11 20:26:03 -0600

    arrays

--- code-along-assignment/arrays.php
+++ code-along-assignment/arrays.php
@@ -1,7 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Document</title>
+    <title>Arrays</title>
   </head>
-  <body></body>
+  <body>
+<?php
+$numbers = array(4,8,15,16,23,42);
+echo $numbers[0];//arrays start at 0
+?>
+<?php $mixed = array(6, "fox", "dog", array("x","y","z"));?><br/>
+<?php echo $mixed[2];?><br/>
+<?php //echo $mixed[3];?><br/>
+<?php //echo $mixed; ?><br/>
+
+<?php echo $mixed[3][1];?><br/>
+
+<?php $mixed[2] = "cat";//set position 2 to cat?>
+<?php $mixed[4] = "mouse";//put item into the fourth array position?>
+<?php $mixed[] = "horse";//sets horse to the 5th array position or last position?>
+
+<pre>
+<?php echo print_r($mixed)?><?php?>
+</pre>
+
+
+<?php
+//php 5.4 added the short array syntax
+$array = [1,2,3];
+?>
+
+  </body>
 </html>
