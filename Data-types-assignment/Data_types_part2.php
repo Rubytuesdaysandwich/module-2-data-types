acac847f79b4d72155de25ed33915073cfada17c
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-05-12 16:02:28 -0600

    data types assignment update

---
+++ Data-types-assignment/Data_types_part2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Data types part 2</title>
+  </head>
+  <body>
+    <?php
+    $x=10;
+    $y=7;
+    ?>
+
+    <?php echo $x + $y;?><br/>
+    <?php echo $x - $y;?><br/>
+    <?php echo $x * $y;?><br/>
+    <?php echo $x / $y;?><br/>
+    <?php echo $x % $y;?><br/>
+
+
+
+  </body>
+</html>
---
+++ Data-types-assignment/data_types_part1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>data-types assignment</title>
+  </head>
+  <body>
+      Part 1:Outputing data
+      <br/>
+    1. <?php echo"Twinkle, Twinkle little star";//sends the string to the browser?><br/>
+
+    2. <?php 
+    $twinkle1 = "Twinkle";
+    $twinkle2 = "star";
+    //giving each variable a assignment
+    ?><br/>
+
+    3. <?php echo "{$twinkle1},{$twinkle1} little {$twinkle2}";//sending the text to the browser and the variable are being used in place of text?><br/>
+
+    4.<?php
+    $twinkle1 = "red" ;
+    $twinkle2 = "rocket-ship";
+    //giving each variable an assignment
+    ?><br/>
+
+    5.<?php echo "{$twinkle1},{$twinkle1} little {$twinkle2}";//sending the text to the browser and the variable are being used in place of text?><br/>
+
+
+  </body>
+</html>
