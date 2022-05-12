5546fe28962be09a0a1685f8b20a312e9cf5a6f1
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-05-12 12:35:08 -0600

    Null and Empty

---
+++ code-along-assignment/null.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>NULL</title>
+  </head>
+  <body>
+<?php
+$var1 = null;
+$var2 = "";
+
+?>
+var1 null? <?php echo is_null($var1);?><br/>
+var2 null? <?php echo is_null($var2);?><br/>
+var3 null? <?php echo is_null($var3);?><br/>
+
+  </body>
+</html>
