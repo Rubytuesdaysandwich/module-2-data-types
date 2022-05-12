0a17635f85b0973fc88b2ba990f562908d076dd7
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-05-12 16:42:42 -0600

    data types part 3 arithmetic operators

--- Data-types-assignment/data_types_part3.php
+++ Data-types-assignment/data_types_part3.php
@@ -1,7 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Document</title>
+    <title>Data-types part 3</title>
   </head>
-  <body></body>
+  <body>
+<!--original:--><?php $variable = 8 ;  echo "value is now {$variable}";//original value starting point?><br/>
+<!--add 2:--><?php $variable += 2; echo "value is now {$variable}";//solves for in place addition short version for concat?><br/>
+<!--subtract 4:--><?php $variable -= 4; echo "value is now {$variable}";//solves for in place subtraction short version for concat?><br/>
+<!--multiply 5:--><?php $variable *= 5; echo "value is now {$variable}";//solves for in place multiplication short version for concat?><br/>
+<!--divide 3:--><?php $variable /= 3; echo "value is now {$variable}";//solves for in place divide short version for concat?><br/>
+<br/>
+<!--increment:--><?php $variable ++; echo "value is now {$variable}";//incrementing by 1?><br/>
+<!--decrement:--><?php $variable --; echo "value is now {$variable}";//decrementing by 1?><br/>
+<br/>
+  </body>
 </html>
